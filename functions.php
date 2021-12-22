b5e421be9171d1d8ef15a4d35889d9dde71d5ee0
Emanuel Jankovec <emanuel.jankovec@zyrgon.com>
2021-12-22 14:43:29 +0100

    first commit

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+
+add_action( 'send_headers', 'tgm_io_strict_transport_security' );
+function tgm_io_strict_transport_security() {
+    //header('X-Frame-Options: SAMEORIGIN');
+    // header('X-XSS-Protection: 1; mode=block');
+    // header('X-Content-Type-Options: nosniff');
+    // header( 'Strict-Transport-Security: max-age=10886400; includeSubDomains; preload' );
+}
+
+add_action('wp_head', 'insert_header');
+function insert_header() {
+}
+
+add_action( 'wp_footer', 'scripts_footer' , 50);
+function scripts_footer(){
+}
+
+add_action( 'wp_enqueue_scripts', 'custom_scripts' , 50);
+function custom_scripts() {
+}
+
+
+
